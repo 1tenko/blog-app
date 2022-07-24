3cfd9c0a3d720e77e4c0fd3ec6c1a69f27827046
tenko <tajm.ogunbiyi@gmail.com>
2022-07-24 12:44:22 +0100

    :construction_worker: Added validation for the inputs to signup

--- src/resolvers/Mutation/auth.ts
+++ src/resolvers/Mutation/auth.ts
@@ -1,4 +1,5 @@
 import { Context } from "../../index";
+import validator from "validator";
 
 interface SignupArgs {
   email: string;
@@ -7,18 +8,69 @@ interface SignupArgs {
   password: string;
 }
 
+interface UserPayload {
+  userErrors: {
+    message: string;
+  }[];
+  user: null;
+}
+
 export const authResolvers = {
-  signup: (
+  signup: async (
     _: any,
     { email, name, password, bio }: SignupArgs,
     { prisma }: Context
-  ) => {
-    return prisma.user.create({
-      data: {
-        email,
-        name,
-        password,
-      },
+  ): Promise<UserPayload> => {
+    const isEmail = validator.isEmail(email);
+
+    if (!isEmail) {
+      return {
+        userErrors: [
+          {
+            message: "invalid email",
+          },
+        ],
+        user: null,
+      };
+    }
+
+    const isValidPassword = validator.isLength(password, {
+      min: 5,
     });
+
+    if (!isValidPassword) {
+      return {
+        userErrors: [
+          {
+            message: "invalid password",
+          },
+        ],
+        user: null,
+      };
+    }
+
+    if (!name || !bio) {
+      return {
+        userErrors: [
+          {
+            message: "invalid name or bio",
+          },
+        ],
+        user: null,
+      };
+    }
+
+    return {
+      userErrors: [],
+      user: null,
+    };
+
+    // return prisma.user.create({
+    //   data: {
+    //     email,
+    //     name,
+    //     password,
+    //   },
+    // });
   },
 };
